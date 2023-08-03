16f9ae4c6296eb64cc4c08b14864853bf1a2d243
Jair Orlando Huaman Bellido <huamanbellidoj@gmail.com>
2023-08-03 10:23:02 -0500

    feat: add tokens into auth response

--- src/functions/sign-in.ts
+++ src/functions/sign-in.ts
@@ -8,13 +8,14 @@ import { CustomError } from "../core/errors/CustomErorr";
 export const handler = async (event: APIGatewayEvent) => {
   try {
     const payload = JSON.parse(event.body) as IAuthenticationAdministrator;
-    await DIContainer.resolve(AuthenticationAdministratorUseCase).execute(
+    const res = await DIContainer.resolve(AuthenticationAdministratorUseCase).execute(
       payload
     );
+    
 
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: "Successfully authorized" }),
+      body: JSON.stringify({ ...res }),
     };
   } catch (error: CustomError | any) {
     return {
